698cd4727321753177e9bfe1ad7273e487666344
Gxlopino <yusgallop@gmail.com>
2026-09-25 09:35:15 -0500

    examen casi terminado

--- admin/configs/routes.js
+++ admin/configs/routes.js
@@ -2,6 +2,7 @@
 import { Router } from 'express';
 import * as admins from '../controllers/admin_controllers.js';
 import * as carrers from '../controllers/carrer_controllers.js';
+import * as student from '../controllers/student_controllers.js';
 import * as nationApis from '../apis/nations_apis.js';
 import { redirectIfAuthenticated, requireAuth } from '../../configs/middlewares.js'; 
 
@@ -22,4 +23,6 @@ router.put('/api/v1/nations/:id', nationApis.updateNation);
 router.delete('/api/v1/nations/:id', nationApis.deleteNation);
 // carrers
 router.get('/admin/carrers', carrers.home);
+// alumnos
+router.get('/admin/students', student.home);
 export default router;
